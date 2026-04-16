125575e380ce68bfa1073c76ae0f358931ed36cc
Jordan <jordan@smartdust.me>
2026-04-16 13:56:45 +0200

    fix activation problem

--- activate.sh
+++ activate.sh
@@ -1,2 +1,9 @@
 #!/bin/env zsh
-source ~/.config/jordan_dotfiles/.zshrc
+
+if [ -z "$TMUX"  ]; then 
+  tmux attach-session -t "jordan" 
+else
+  tmux new-session -d -s "jordan" 'source ~/.config/jordan_dotfiles/.zshrc'
+  exec tmux attach -t "jordan"
+fi
+
