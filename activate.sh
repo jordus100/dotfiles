825a7b24c9935b30a2b27115ed39d325454eebd8
Jordan <jordan@smartdust.me>
2026-04-16 15:58:37 +0200

    Fix logic in activate

--- activate.sh
+++ activate.sh
@@ -1,8 +1,8 @@
 #!/bin/env zsh
 
 if [ -z "$TMUX"  ]; then 
-  tmux attach-session -t "jordan" 
-else
   exec tmux new-session -s "jordan" "~/.config/jordan_dotfiles/zshi.sh 'source ~/.config/jordan_dotfiles/.zshrc'"
+else
+  tmux attach-session -t "jordan" 
 fi
 
