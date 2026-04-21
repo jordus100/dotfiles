1903741988399bd3486056f846bfed80e132c96b
Jordan <jordan@smartdust.me>
2026-04-21 13:35:27 +0200

    cosmetic

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,29 +6,28 @@ REPO_URL=https://github.com/jordus100/dotfiles
 declare -a Programs=('git' 'tmux')
 
 for prog in "${Programs[@]}"; do
-  if ! command -v ${prog} >/dev/null 2>&1
-  then
-      echo "Instaling ${prog}"
-      sudo apt install -y ${prog}
+  if ! command -v ${prog} >/dev/null 2>&1; then
+    echo "Instaling ${prog}"
+    sudo apt install -y ${prog}
   fi
 done
 
 if [ -d "$DOTFILES_DIR/.git" ]; then
   git -C "$DOTFILES_DIR" pull origin main --quiet
 else
-  git clone --quiet --depth 1 "$REPO_URL" "$DOTFILES_DIR"
+  git clone --depth 1 "$REPO_URL" "$DOTFILES_DIR"
 fi
 
 if [[ ! $(which zsh) ]]; then
-    echo "Instaling zsh"
-    sudo apt install -y zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    chsh -s /usr/bin/zsh
+  echo "Instaling zsh"
+  sudo apt install -y zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  chsh -s /usr/bin/zsh
 fi
 git clone https://github.com/jeffreytse/zsh-vi-mode \
   ~/.oh-my-zsh/plugins/zsh-vi-mode
 if [[ ! $(grep "alias 'jordan" ~/.zshrc) ]]; then
-  echo "alias 'jordan=source ${DOTFILES_DIR}/activate.sh'" >> ~/.zshrc
+  echo "alias 'jordan=source ${DOTFILES_DIR}/activate.sh'" >>~/.zshrc
 fi
 
 zsh -i
